9fab4bfd60c408ff5fb341958fb940cf06a8a133
Ben Draut <drautb@gmail.com>
2019-09-28 15:03:21 -0600

    Initial commit

---
+++ precache-manifest.050a01e8b8c206d8842bde763c51f113.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1275eac2c4d7fdb7fb351a1f26d16dcc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01f1ee9ceb9414bf9e13",
+    "url": "/static/css/main.80afcbb8.chunk.css"
+  },
+  {
+    "revision": "1f460a643b16f3aa0aa7",
+    "url": "/static/js/2.8d223f6d.chunk.js"
+  },
+  {
+    "revision": "01f1ee9ceb9414bf9e13",
+    "url": "/static/js/main.df522b26.chunk.js"
+  },
+  {
+    "revision": "625c3c645345f67f95f8",
+    "url": "/static/js/runtime-main.13e12990.js"
+  }
+]);
